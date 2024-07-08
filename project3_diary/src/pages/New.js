81e3940d9ae8c0da075cc620f3d5e3ede615ff80
Gayeon Lee <gayean430@gmail.com>
2024-07-09 00:07:57 +0900

    Add project3

---
+++ project3_diary/src/pages/New.js
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../component/Button";
+import Header from "../component/Header";
+import Editor from "../component/Editor";
+import { useContext } from "react";
+import Diary from "./Diary";
+import { DiaryDispatchContext } from "../App";
+
+const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+      />
+      <Editor />
+    </div>
+  );
+};
+
+export default New;
